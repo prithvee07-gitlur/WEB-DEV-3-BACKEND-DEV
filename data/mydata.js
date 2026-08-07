e81fa1a36a3eea06226144f72d8ece71dc2132e4
Prithvee Singh Yadav <prithveeyadav06@gmail.com>
2026-08-07 10:52:04 +0530

    added the changes made in the data folder

---
+++ data/mydata.js
@@ -0,0 +1,7 @@
+const cities=[
+    "Goa","Delhi","Mumbai","Jalandhar",
+    "Ludhiana","Hydrabad","Chennai","Goa"
+]
+
+module.exports = cities;   // example of the internal module
+// to export multiple entities export it with in the curly brackets "{}"
